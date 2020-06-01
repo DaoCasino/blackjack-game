672db6a8f0490a123bb527e408cb38757cb25365
Emil Guseynov <theluckyemil@gmail.com>
2020-06-01 16:29:20 +0300

    Game skeleton

--- contracts/include/blackjack/blackjack.hpp
+++ contracts/include/blackjack/blackjack.hpp
@@ -5,19 +5,59 @@
 
 namespace blackjack {
 
-using eosio::checksum256;
 using eosio::name;
+using eosio::asset;
+using bytes = std::vector<char>;
+using eosio::checksum256;
+using eosio::check;
+using card_game::card;
+using game_sdk::param_t;
+
+namespace param {
+    const uint16_t min_ante = 0;
+    const uint16_t max_ante = 1;
+    const uint16_t max_payout = 2;
+}
+
+namespace action {
+    const uint16_t bet = 0;
+    const uint16_t play = 1;
+}
+
+namespace decision {
+    const uint16_t insure = 0;
+    const uint16_t split = 1;
+    const uint16_t double_down = 2;
+}
 
 class [[eosio::contract]] blackjack: public game_sdk::game {
+
+public:
+    struct [[eosio::table("bet")]] bet_row {
+        uint64_t ses_id;
+        asset ante;
+
+        uint64_t primary_key() const { return ses_id; }
+
+        EOSLIB_SERIALIZE(bet_row, (ses_id)(ante))
+    };
+    using bet_table = eosio::multi_index<"bet"_n, bet_row>;
 public:
     blackjack(name receiver, name code, eosio::datastream<const char*> ds):
-        game(receiver, code, ds) {}
+        game(receiver, code, ds),
+        bets(_self, _self.value) {}
 
     void on_new_game(uint64_t ses_id) override final;
     void on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param_t> params) override final;
     void on_random(uint64_t ses_id, checksum256 rand) override final;
     void on_finish(uint64_t ses_id) override final;
 
+    void check_params(uint64_t ses_id) const;
+    void check_bet(uint64_t ses_id, const param_t& ante_bet) const;
+    param_t get_and_check(uint64_t ses_id, uint16_t param, const std::string& error_msg) const;
+
+private:
+    bet_table bets;
 };
 
 } // ns blackjack
--- contracts/src/blackjack.cpp
+++ contracts/src/blackjack.cpp
@@ -4,15 +4,66 @@
 
 namespace blackjack {
 
-using card_game::card;
+param_t blackjack::get_and_check(uint64_t ses_id, uint16_t param, const std::string& error_msg) const {
+    const auto res = get_param_value(ses_id, param);
+    if (res != std::nullopt) {
+        return *res;
+    }
+    eosio::check(false, error_msg);
+    return 0;
+}
+
+void blackjack::check_params(uint64_t ses_id) const {
+    const auto min_ante_bet = get_and_check(ses_id, param::min_ante, "min ante bet is absent");
+    const auto max_ante_bet = get_and_check(ses_id, param::max_ante, "max ante bet is absent");
+    const auto max_payout = get_and_check(ses_id, param::max_payout, "max payout is absent");
+    check(max_ante_bet >= min_ante_bet, "max ante bet is less than min");
+    check(min_ante_bet <= get_session(ses_id).deposit.amount, "deposit is less than min bet");
+    check(max_payout >= get_session(ses_id).deposit.amount, "deposit exceeds max payout");
+}
+
+void blackjack::check_bet(uint64_t ses_id, const param_t& ante) const {
+    check(*get_param_value(ses_id, param::min_ante) <= ante, "ante bet is less than min");
+    check(*get_param_value(ses_id, param::max_ante) >= ante, "ante bet is more than max");
+    check(2 * ante == get_session(ses_id).deposit.amount, "max loss is more than deposit");
+}
 
 void blackjack::on_new_game(uint64_t ses_id) {
+    check_params(ses_id);
+    require_action(action::bet);
 }
 
 void blackjack::on_action(uint64_t ses_id, uint16_t type, std::vector<game_sdk::param_t> params) {
+    if (type == action::bet) {
+        check(params.size() == 1, "invalid param size");
+        check_bet(ses_id, params[0]);
+
+        bets.emplace(get_self(), [&](auto& row) {
+            row.ses_id = ses_id;
+            row.ante = asset(params[0], core_symbol);
+        });
+        // generate initial cards
+        require_random();
+    } else if (type == action::play) {
+        check(params.size() == 1, "invalid param size");
+        switch (params[0]) {
+            case decision::insure:
+                return;
+            case decision::split:
+                return;
+            case decision::double_down:
+                return;
+            default:
+                check(0, "invalid decision");
+        }
+    } else {
+        check(0, "invalid action");
+    }
 }
 
 void blackjack::on_random(uint64_t ses_id, checksum256 rand) {
+    // STUB
+    require_action(action::play);
 }
 
 void blackjack::on_finish(uint64_t ses_id) {
